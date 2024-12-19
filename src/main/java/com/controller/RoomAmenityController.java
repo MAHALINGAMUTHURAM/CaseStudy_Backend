ba3912ac87bd5cc54578c77085ba7cfdf4ba3644
ARUMUGAM <meenakshi.arumugam@capgemini.com>
2024-12-19 12:40:12 +0530

    first

--- src/main/java/com/controller/RoomAmenityController.java
+++ src/main/java/com/controller/RoomAmenityController.java
@@ -1,33 +1,39 @@
 package com.controller;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
-
+ 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+ 
 import com.model.RoomAmenity;
-
+ 
 import com.service.RoomAmenityService;
-
+ 
 @RestController
 @RequestMapping("/api/roomAmenity")
 public class RoomAmenityController {
-
+ 
     @Autowired
     private RoomAmenityService roomAmenityService;
-
-
+ 
+ 
     @PostMapping("/post")
     public ResponseEntity<Object> createRoomAmenity(@RequestBody RoomAmenity roomAmenity) {
         try {
             if (roomAmenityService.findRoomAmenity(roomAmenity)) {
-                return ResponseEntity.badRequest().body("{\"code\": \"ADDFAILS\", \"message\": \"RoomAmenity already exist\"}");
+                return ResponseEntity.badRequest().body("{\"code\": \"ADDFAILS\", \"message\": \"RoomAmenity already exists\"}");
             }
+ 
+          
             roomAmenityService.saveRoomAmenity(roomAmenity);
             return ResponseEntity.ok("{\"code\": \"POSTSUCCESS\", \"message\": \"RoomAmenity added successfully\"}");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid argument: " + e.getMessage()); 
+            return ResponseEntity.badRequest().body("{\"code\": \"INVALIDARGUMENT\", \"message\": \"Invalid RoomAmenity data\"}");
         } catch (Exception e) {
-        	System.out.println(e);
-            return ResponseEntity.status(500).body("{\"code\": \"ADDFAILS\", \"message\": \"RoomAmenity already exist\"}");
+            System.out.println("Unexpected error: " + e); 
+            return ResponseEntity.status(500).body("{\"code\": \"ADDFAILS\", \"message\": \"Error adding RoomAmenity\"}");
         }
     }
+ 
 }
--- src/main/java/com/model/Review.java
+++ src/main/java/com/model/Review.java
@@ -2,6 +2,7 @@ package com.model;
 
 import java.sql.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,11 +19,18 @@ public class Review {
 	
 	@OneToOne
 	@JoinColumn(name="reservation_id")
+	
 	private Reservation reservation;
 	
+	@Column( nullable = false)
 	private int rating;
+	
+	@Column( nullable = false)
 	private String comment;
+	
+	@Column( nullable = false)
 	private Date review_date;
+	
 	public long getReview_id() {
 		return review_id;
 	}
