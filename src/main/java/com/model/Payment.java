4b063bd8f5c2c9d645445e7706a350fb9d98c764
S <amiritavarshini.s@capgemini.com>
2024-12-19 12:26:40 +0530

    hi

--- src/main/java/com/model/Payment.java
+++ src/main/java/com/model/Payment.java
@@ -2,6 +2,7 @@ package com.model;
 
 import java.sql.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,8 +20,11 @@ public class Payment {
 	@OneToOne
 	@JoinColumn(name="reservation_id")
 	private Reservation reservation;
+	@Column( nullable = false)
 	private double amount;
+	@Column( nullable = false)
 	private Date payment_date;
+	@Column( nullable = false)
 	private String paymentStatus;
 	public long getPayment_id() {
 		return payment_id;
